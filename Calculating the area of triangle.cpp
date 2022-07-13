d24d3210e294237b801c8984f84c50320a17f00b
TSHEPISO MASIPA <104618398+tshepiso19m@users.noreply.github.com>
2022-07-13 08:21:06 +0200

    Add files via upload

---
+++ Calculating the area of triangle.cpp
@@ -0,0 +1,40 @@
+// Calculating the area of triangle.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+double TriArea(double a, double b, double c) {
+
+	double s;
+	s = (a + b + c) / 2;
+
+	return sqrt(s * (s - a) * (s - b) * (s - c));
+
+}
+int main() {
+
+	double a, b, c;
+
+	cout << "Enter length of three sides of a triangle: " << endl;
+	cin >> a >> b >> c;
+
+	cout << "The area is equal to " << TriArea(a, b, c);
+	cout << endl;
+
+	return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
